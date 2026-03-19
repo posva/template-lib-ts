c68c0a0dbe87928d82923e4202d3df82c813fd17
Eduardo San Martin Morote <posva13@gmail.com>
2026-03-19 16:21:44 +0100

    chore: initial project setup

---
+++ tsdown.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'tsdown'
+import type { UserConfig } from 'tsdown'
+
+const commonOptions = {
+  sourcemap: true,
+  format: ['esm'],
+  deps: {
+    onlyAllowBundle: [],
+    neverBundle: ['vue'],
+  },
+  target: 'esnext',
+  tsconfig: 'tsconfig.build.json',
+  dts: true,
+} satisfies UserConfig
+
+export default defineConfig([
+  {
+    ...commonOptions,
+    clean: true,
+    entry: ['src/index.ts'],
+    globalName: 'PosvaTemplateLib',
+  },
+])
