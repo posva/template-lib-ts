640ec91c5d65352fdeebc3b6ecae1cbacec77fd7
Eduardo San Martin Morote <posva13@gmail.com>
2026-04-10 15:33:04 +0200

    build: enable oxc for dts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -10,7 +10,14 @@ const commonOptions = {
   },
   target: 'esnext',
   tsconfig: 'tsconfig.build.json',
-  dts: true,
+  dts: {
+    enabled: true,
+    // NOTE: if you cannot use isolatedDeclarations, this makes writing types
+    // bit harder but makes the generation way faster. Disable if you can't
+    // explicitely type all exported values.
+    // See https://github.com/microsoft/TypeScript/issues/58944#issuecomment-4213203205
+    oxc: true,
+  },
   // sets package.json "exports" field to the generated files
   exports: true,
 } satisfies UserConfig
